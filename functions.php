c7c943960712e35171bee08d38035961b927aa4e
Ranga Mapuvire <avanna23@gmail.com>
2012-03-27 05:47:07 -0500

    just before widgets

--- functions.php
+++ functions.php
@@ -746,14 +746,14 @@ return $output;
 
 add_shortcode('ks-events-full', 'ks_events_full'); // You can now call onto this shortcode with [ks-events-full limit='20']
 
-function twentyeleven_comment( $comment, $args, $depth ) {
+function bendo_comment( $comment, $args, $depth ) {
 	$GLOBALS['comment'] = $comment;
 	switch ( $comment->comment_type ) :
 		case 'pingback' :
 		case 'trackback' :
 	?>
 	<li class="post pingback">
-		<p><?php _e( 'Pingback:', 'twentyeleven' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?></p>
+		<p><?php _e( 'Pingback:', 'bendo' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( 'Edit', 'bendo' ), '<span class="edit-link">', '</span>' ); ?></p>
 	<?php
 			break;
 		default :
@@ -770,22 +770,22 @@ function twentyeleven_comment( $comment, $args, $depth ) {
 						echo get_avatar( $comment, $avatar_size );
 
 						/* translators: 1: comment author, 2: date and time */
-						printf( __( '<span>%1$s %2$s</span>', 'twentyeleven' ),
+						printf( __( '<span>%1$s %2$s</span>', 'bendo' ),
 							sprintf( '<span class="fn">%s</span>', get_comment_author_link() ),
 							sprintf( '<a class="comment-time" href="%1$s"><time pubdate datetime="%2$s">%3$s</time></a>',
 								esc_url( get_comment_link( $comment->comment_ID ) ),
 								get_comment_time( 'c' ),
 								/* translators: 1: date, 2: time */
-								sprintf( __( '%1$s', 'twentyeleven' ), get_comment_date('F d Y'))
+								sprintf( __( '%1$s', 'bendo' ), get_comment_date('F d Y'))
 							)
 						);
 					?>
 
-					<?php edit_comment_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+					<?php edit_comment_link( __( 'Edit', 'bendo' ), '<span class="edit-link">', '</span>' ); ?>
 				</div><!-- .comment-author .vcard -->
 
 				<?php if ( $comment->comment_approved == '0' ) : ?>
-					<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'twentyeleven' ); ?></em>
+					<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'bendo' ); ?></em>
 					<br />
 				<?php endif; ?>
 
@@ -794,7 +794,7 @@ function twentyeleven_comment( $comment, $args, $depth ) {
 			<div class="comment-content"><?php comment_text(); ?></div>
 
 			<div class="reply">
-				<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply <span>&darr;</span>', 'twentyeleven' ), 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+				<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply <span>&darr;</span>', 'bendo' ), 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
 			</div><!-- .reply -->
 		</article><!-- #comment-## -->
 
@@ -803,14 +803,14 @@ function twentyeleven_comment( $comment, $args, $depth ) {
 	endswitch;
 }
 
-function twentyeleven_content_nav( $nav_id ) {
+function bendo_content_nav( $nav_id ) {
 	global $wp_query;
 
 	if ( $wp_query->max_num_pages > 1 ) : ?>
 		<nav id="<?php echo $nav_id; ?>">
-			<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyeleven' ) ); ?></div>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></div>
+			<h3 class="assistive-text"><?php _e( 'Post navigation', 'bendo' ); ?></h3>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'bendo' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'bendo' ) ); ?></div>
 		</nav><!-- #nav-above -->
 	<?php endif;
 }
--- single.php
+++ single.php
@@ -37,23 +37,23 @@
 									<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
 									<?php
 										/* translators: used between list items, there is a space after the comma */
-										$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
+										$categories_list = get_the_category_list( __( ', ', 'bendo' ) );
 										if ( $categories_list ):
 									?>
 									<span class="cat-links">
-										<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
+										<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'bendo' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
 										$show_sep = true; ?>
 									</span>
 									<?php endif; // End if categories ?>
 									<?php
 										/* translators: used between list items, there is a space after the comma */
-										$tags_list = get_the_tag_list( '', __( ', ', 'twentyeleven' ) );
+										$tags_list = get_the_tag_list( '', __( ', ', 'bendo' ) );
 										if ( $tags_list ):
 										if ( $show_sep ) : ?>
 									<span class="sep"> | </span>
 										<?php endif; // End if $show_sep ?>
 									<span class="tag-links">
-										<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list );
+										<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'bendo' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list );
 										$show_sep = true; ?>
 									</span>
 									<?php endif; // End if $tags_list ?>
@@ -63,10 +63,10 @@
 									<?php if ( $show_sep ) : ?>
 									<span class="sep"> | </span>
 									<?php endif; // End if $show_sep ?>
-									<span class="comments-link"><?php comments_popup_link( '<span class="leave-reply">' . __( 'Comment', 'twentyeleven' ) . '</span>', __( '<b>1</b> Reply', 'twentyeleven' ), __( '<b>%</b> Replies', 'twentyeleven' ) ); ?></span>
+									<span class="comments-link"><?php comments_popup_link( '<span class="leave-reply">' . __( 'Comment', 'bendo' ) . '</span>', __( '<b>1</b> Reply', 'bendo' ), __( '<b>%</b> Replies', 'bendo' ) ); ?></span>
 									<?php endif; // End if comments_open() ?>
 
-									<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+									<?php edit_post_link( __( 'Edit', 'bendo' ), '<span class="edit-link">', '</span>' ); ?>
 								</footer><!-- #entry-meta -->
 								
 					<?php comments_template( '', true ); ?>
